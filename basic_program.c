d0df6224ca4e7536f828967d4f8f636b80c12225
Hasibul Hasan Hasib <hasib.hasan@brainstation-23.com>
2025-03-24 12:58:48 +0600

    Added Fibonacci function

--- basic_program.c
+++ basic_program.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
-
+int factorial(int n) { return (n <= 1) ? 1 : n * factorial(n - 1);\n}
+int fibonacci(int n) {if (n <= 1) return n;\n    return fibonacci(n - 1) + fibonacci(n - 2);}
 int main() {
     printf("Hello, World!\n");
     return 0;
 }
-\nint factorial(int n) {\n    return (n <= 1) ? 1 : n * factorial(n - 1);\n}
+
